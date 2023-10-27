5688992516168d98788613fed04c5a287ef0356d
StrunkGroove <jmarkj@mail.ru>
2023-10-27 16:23:50 +0300

    switch on count in 3 links

--- app/main/celery.py
+++ app/main/celery.py
@@ -9,35 +9,35 @@ app.autodiscover_tasks()
 
 
 app.conf.beat_schedule = {
-    # 'count-links-3-task-binance': {
-    #     'task': 'count_links_3.tasks.binance',
-    #     'schedule': 20.0,
-    # },
-    # 'count-links-3-task-bybit': {
-        # 'task': 'count_links_3.tasks.bybit',
-        # 'schedule': 20.0,
-    # },
-    # 'count-links-3-task-kucoin': {
-    #     'task': 'count_links_3.tasks.kucoin',
-    #     'schedule': 20.0,
-    # },
-    # 'count-links-3-task-huobi': {
-    #     'task': 'count_links_3.tasks.huobi',
-    #     'schedule': 20.0,
-    # },
-    # 'count-links-3-task-okx': {
-    #     'task': 'count_links_3.tasks.okx',
-    #     'schedule': 20.0,
-    # },
+    'count-links-3-task-binance': {
+        'task': 'count_links_3.tasks.binance',
+        'schedule': 20.0,
+    },
+    'count-links-3-task-bybit': {
+        'task': 'count_links_3.tasks.bybit',
+        'schedule': 20.0,
+    },
+    'count-links-3-task-kucoin': {
+        'task': 'count_links_3.tasks.kucoin',
+        'schedule': 20.0,
+    },
+    'count-links-3-task-huobi': {
+        'task': 'count_links_3.tasks.huobi',
+        'schedule': 20.0,
+    },
+    'count-links-3-task-okx': {
+        'task': 'count_links_3.tasks.okx',
+        'schedule': 20.0,
+    },
 
-    # 'best-change-parsing-task': {
-    #     'task': 'best_change_parsing.tasks.main',
-    #     'schedule': 30.0,
-    # },
-    # 'best-change-zip-task': {
-    #     'task': 'best_change_zip.tasks.main',
-    #     'schedule': 30.0,
-    # },
+    'best-change-parsing-task': {
+        'task': 'best_change_parsing.tasks.main',
+        'schedule': 30.0,
+    },
+    'best-change-zip-task': {
+        'task': 'best_change_zip.tasks.main',
+        'schedule': 30.0,
+    },
 
     'count-task': {
         'task': 'count.tasks.main',
