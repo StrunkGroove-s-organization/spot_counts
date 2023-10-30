58ee4df04a38eceb2cff88378bca94a45e6c9be7
StrunkGroove <jmarkj@mail.ru>
2023-10-30 14:48:50 +0300

    try

--- app/count/services.py
+++ app/count/services.py
@@ -351,17 +351,13 @@ class CountInThree(Count):
                     spread = self.calculate_spread(price_buy, best_price, price_sell)
                     if spread < 0.2: continue
 
-                    if ad_buy.get('fake') is True:
-                        price_buy = 1 / price_buy
-
-                    if ad_sell.get('fake') is True:
-                        price_sell = 1 / price_sell
-
                     exchange_id = best['exchange_id']
                     hashed = self.create_hash(
                         self.ex, base_buy, best_base_num, best_quote_num, best_id
                     )
 
+                    if ad_buy.get('fake') is True:
+                        price_buy = 1 / price_buy
 
                     record = {
                         "first": {
